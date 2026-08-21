85c2a0a5af079d2d36e8995cd486331894d9339f
Emirhan <emirhan551952@gmail.com>
2026-08-21 10:42:06 +0300

    fix(google): order virtual server migration

---
+++ apps/backend/drizzle/0022_google_workspace_virtual_constraint.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "mcp_servers" DROP CONSTRAINT IF EXISTS "mcp_servers_url_check";
+--> statement-breakpoint
+ALTER TABLE "mcp_servers" ADD CONSTRAINT "mcp_servers_url_check" CHECK (
+  (type = 'SSE' AND url IS NOT NULL AND command IS NULL AND url ~ '^https?://[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(:[0-9]+)?(/[a-zA-Z0-9-._~:/?#\[\]@!$&''()*+,;=]*)?$') OR
+  (type = 'STDIO' AND url IS NULL AND command IS NOT NULL) OR
+  (type = 'STREAMABLE_HTTP' AND url IS NOT NULL AND command IS NULL AND url ~ '^https?://[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(:[0-9]+)?(/[a-zA-Z0-9-._~:/?#\[\]@!$&''()*+,;=]*)?$') OR
+  (type = 'VIRTUAL' AND url IS NULL AND command IS NULL)
+);
